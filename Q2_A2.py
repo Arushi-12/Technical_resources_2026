3cd46267afb26c57332b1fbbf17be1c497454f84
Arushi-12 <118588760+Arushi-12@users.noreply.github.com>
2023-01-12 00:38:40 +0530

    Submitting q2 of Assignment1

---
+++ Q2_A2.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the marks: "))
+if n>90:
+  print("Excellent!")
+elif n>80:
+  print('Good')
+elif n>70:
+  print('Fair')
+elif n>60:
+  print('Meets expectations')
+else:
+  print('Par')
